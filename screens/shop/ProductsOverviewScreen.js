fc4847e6ef084b848a1f4ce8c70272aaf1b63b28
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-04-27 14:26:28 +0300

    basic navigation

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-import {View, FlatList, Text, } from 'react-native'
+import { View, FlatList, Text, } from 'react-native'
+import { useSelector } from 'react-redux';
 
-const ProductsOverviewScreen = props =>{
+const ProductsOverviewScreen = props => {
+
+	const products = useSelector(state => state.products.availableProducts)
 
 	return (
-		<FlatList/>
+		<FlatList
+			data={products}
+			keyExtractor={item => item.id}
+			renderItem={itemData => <Text>{itemData.item.title}</Text>}
+		/>
 	)
+}
+
+ProductsOverviewScreen.navigationOptions = {
+	headerTitle: 'All products'
 }
 
 export default ProductsOverviewScreen
