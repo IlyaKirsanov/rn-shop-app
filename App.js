a5f1ea1e6cc0dcdb545d1e82d8c2dce1ca8698a4
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-04-27 13:55:44 +0300

    basic state

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import productsReducer from './store/reducers/products'
+
+const rootReducer = combineReducer({
+  products: productsReducer
+})
+
+const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>New app</Text>
-    </View>
+    <Provider store={store}>
+      <View></View>
+    </Provider>
   );
 }
 
