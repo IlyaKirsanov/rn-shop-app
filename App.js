84813cfe309dc389b3e7544fd524d299942cf86b
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-05-04 13:30:24 +0300

    adding custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import productsReducer from './store/reducers/products'
 import ShopNavigator from './navigation/ShopNavigator'
+import AppLoading from 'expo-app-loading'
+import * as Font from 'expo-font'
 
 const rootReducer = combineReducers({
   products: productsReducer
@@ -12,14 +13,33 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer)
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        onError={console.warn}
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
