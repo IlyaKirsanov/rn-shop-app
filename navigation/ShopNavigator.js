fadae50ca5b0e0383dcc18da0bb630dbe54f9cdb
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-05-05 12:58:36 +0300

    added cart btn and screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack'
 import { Platform } from 'react-native'
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
+import CartScreen from '../screens/shop/CartScreen'
 
 const ProductsNavigator = createStackNavigator(
 	{
 		ProductsOverview: ProductsOverviewScreen,
-		ProductDetail: ProductDetailScreen
+		ProductDetail: ProductDetailScreen,
+		Cart: CartScreen
 	}, {
 	defaultNavigationOptions: {
 		headerStyle: {
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-import { FlatList } from 'react-native'
+import { FlatList, Platform } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux';
-import * as cartActions from '../../store/actions/cart'
+import * as cartActions from '../../store/actions/cart';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../../components/UI/HeaderButton'
 
 import ProductItem from '../../components/shop/ProductItem'
 
@@ -32,8 +34,22 @@ const ProductsOverviewScreen = props => {
 	)
 }
 
-ProductsOverviewScreen.navigationOptions = {
-	headerTitle: 'All products'
+ProductsOverviewScreen.navigationOptions = navData => {
+
+	return {
+		headerTitle: 'All products',
+		headerRight: () => {
+			return (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title="Cart"
+					iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+					onPress={() => {navData.navigation.navigate('Cart') }}
+				/>
+			</HeaderButtons>)
+		}
+	}
+
+
 }
 
 export default ProductsOverviewScreen
